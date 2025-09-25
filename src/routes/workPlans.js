ca07e046366a67ab1403a6c17fa5ecb46f9b7de6
DeEphraim <deephraim5@gmail.com>
2025-09-25 18:01:58 -0500

    unit changesfdfs

--- src/routes/workPlans.js
+++ src/routes/workPlans.js
@@ -1,16 +1,17 @@
 const express = require('express');
 const router = express.Router();
-const auth = require('../middleware/auth');
+// Import auth utilities; need the actual middleware function (authMiddleware)
+const { authMiddleware } = require('../middleware/auth');
 const ctrl = require('../controllers/workPlanController');
 
 // All endpoints require auth for now
-router.get('/', auth, ctrl.listWorkPlans);
-router.get('/:id', auth, ctrl.getWorkPlan);
-router.post('/', auth, ctrl.createWorkPlan);
-router.put('/:id', auth, ctrl.updateWorkPlan);
-router.post('/:id/submit', auth, ctrl.submitWorkPlan);
-router.post('/:id/approve', auth, ctrl.approveWorkPlan);
-router.post('/:id/reject', auth, ctrl.rejectWorkPlan);
-router.post('/:id/activity-progress', auth, ctrl.updateActivityProgress);
+router.get('/', authMiddleware, ctrl.listWorkPlans);
+router.get('/:id', authMiddleware, ctrl.getWorkPlan);
+router.post('/', authMiddleware, ctrl.createWorkPlan);
+router.put('/:id', authMiddleware, ctrl.updateWorkPlan);
+router.post('/:id/submit', authMiddleware, ctrl.submitWorkPlan);
+router.post('/:id/approve', authMiddleware, ctrl.approveWorkPlan);
+router.post('/:id/reject', authMiddleware, ctrl.rejectWorkPlan);
+router.post('/:id/activity-progress', authMiddleware, ctrl.updateActivityProgress);
 
 module.exports = router;
